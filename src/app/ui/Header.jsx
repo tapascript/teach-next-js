0bbd959738f5466ae1992b0d55976dd262cc703b
Tapas Adhikary <tapas.adhikary@gmail.com>
2023-12-06 12:58:33 +0530

    header with links

--- src/app/ui/Header.jsx
+++ src/app/ui/Header.jsx
@@ -1,14 +1,14 @@
-
+import Link from "next/link";
 export default function Header() {
   return (
     <header className="flex justify-between items-center px-8 py-4 bg-[#020617] text-white">
       <h1 className="text-lg font-bold"><a href="/">Learn Next.js</a></h1>
       <nav className="flex space-x-6">
-        <a href="/">Home</a>
-        <a href="/blog">Blog</a>
-        <a href="/play">Play</a>
-        <a href="/store">Store</a>
-        <a href="/about">About</a>
+        <Link href="/">Home</Link>
+        <Link href="/blog">Blog</Link>
+        <Link href="/play">Play</Link>
+        <Link href="/store">Store</Link>
+        <Link href="/about">About</Link>
       </nav>
     </header>
   );
