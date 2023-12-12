321f8976ed2658b4daa0ee8bc4d240a4eeabd3b9
Tapas Adhikary <tapas.adhikary@gmail.com>
2023-12-12 09:56:35 +0530

    A few more changes on parallel route

--- src/app/dashboard/@growth/page.js
+++ src/app/dashboard/@growth/page.js
@@ -1,14 +1,15 @@
 import Link from "next/link"
 export default async function Page() {
   return(
-    <div>
-      <h2 className="text-2xl pb-8">Growth</h2>
+    <div className="h-42">
+      <h2 className="text-2xl pb-4">Growth - last 28 days</h2>
       <div className="flex flex-col justify-center items-center text-center">
-      <Link 
-        href="/dashboard/metrics"
-        className="bg-purple-500 text-white p-1 rounded-md text-lg">
-          See Metrics
-      </Link>
+        <p className="text-3xl pb-4">89%</p>
+        <Link 
+          href="/dashboard/metrics"
+          className="bg-purple-500 text-white p-1 rounded-md text-lg">
+            See Metrics
+        </Link>
       </div>
     </div>
   )
